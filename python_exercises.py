418fabcd6e5f809a82ab90d111886d66695e2888
dwitiyabana <dwitiyabana07@gmail.com>
2026-07-08 23:53:09 +0530

    Practice #clock countdown timer code in seconds

--- python_exercises.py
+++ python_exercises.py
@@ -2,7 +2,7 @@
 import time
 x=int(input("enter time in seconds"))
 seconds=x/60
-minutes=x roz maid se kam krwao kapde dhone and then khana banao ya boring akele gaane suno bss will be over
+minutes=(seconds%60)/60
 for a in range(x,0,-1):
     time.sleep(1)
     print(f"the time is 00:{minutes:02}:{a:02}")
