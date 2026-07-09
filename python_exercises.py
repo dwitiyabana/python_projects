61f771ccaf32c03a0c36e60d8180570be278544e
dwitiyabana <dwitiyabana07@gmail.com>
2026-07-09 23:55:30 +0530

    reverse right angle triangle printing

--- python_exercises.py
+++ python_exercises.py
@@ -1,6 +1,6 @@
 #right angle triangle printing
 length=int(input("enter the length"))
-for x in range(length):
+for x in range(length, 0, -1):
     for y in range(x):
         print("@", end=" ")
     print()
