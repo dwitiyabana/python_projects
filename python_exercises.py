b89080b3dc38b099be26b58bd9b5b1800adbecf4
dwitiyabana <dwitiyabana07@gmail.com>
2026-07-09 23:42:00 +0530

    basic pattern printing rectangle and square

--- python_exercises.py
+++ python_exercises.py
@@ -1,10 +1,8 @@
-#clock countdown timer code
-import time
-x=int(input("enter time in seconds"))
-for a in range(x,0,-1):
-    seconds=a%60
-    minutes=int(a/60)%60
-    hrs=int(a/3600)
-    print(f"the time is {hrs:02}:{minutes:02}:{seconds:02}")
-    time.sleep(1)
-print("THE TIMES OVER!!!")
+#rectangle printing
+length=int(input("enter the length"))
+breadth=int(input("enter the breadth"))
+for x in range (breadth):
+    for y in range (length):
+        print("*", end=" ")
+    print()
+    
