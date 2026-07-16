5e045f4a793b7873fc5ce11b45c2368b9ef94ee8
dwitiyabana <dwitiyabana07@gmail.com>
2026-07-16 15:24:53 +0530

    python quiz game using nested loop and nested tuple

--- python_exercises.py
+++ python_exercises.py
@@ -1,6 +1,32 @@
-#basic dialer display matrix program
-a=((1,2,3),(4,5,6),(7,8,9),("*",0,"#"))
-for collection in a:
-    for elements in collection:
-        print(elements, end = " ")
+#python quiz game
+questions = ("How many elements are in the periodic table?: ",
+              "Which animal lays the largest eggs?: ",
+              "What is the most abundant gas in Earth's atmosphere?: ",
+              "How many bones are in the human body?: ",
+              "Which planet in the solar system is the hottest?: ")
+
+options =   (("Α. 116", "B. 117", "C. 118", "D. 119"),
+             ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
+             ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"),
+             ("Α. 206", "B. 207", "C. 208", "D. 209"),
+             ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
+
+answers = ("c","d","a","a","b")
+guesses = []
+score = 0
+question_num = 0
+
+for question in questions:
+    print(question)
+    for option in options[question_num]:
+        print(option)
+    guess=input("enter the answer (A, B, C, D)").lower()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score+=1
+        print("correct")
+    else:
+        print("incorrect")
+        print(f"the correct answer is {answers[question_num].upper()}")
+    question_num += 1
     print()
