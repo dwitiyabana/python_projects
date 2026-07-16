0b1bfe0dd7e6264b0d2078adb844954f19ba1464
dwitiyabana <dwitiyabana07@gmail.com>
2026-07-16 14:34:35 +0530

    basic dialer display matrix program

--- python_exercises.py
+++ python_exercises.py
@@ -1,18 +1,6 @@
-#shopping cart program
-food=[]
-price=[]
-total = 0
-while True:
-    foods=input("enter the food you wanna eat (q to quit) : ")
-    if foods.lower() == "q" :
-        break
-    else:
-        prices=float(input("enter the prices for the food : $"))
-        food.append(foods)
-        price.append(prices)
-print("the shopping cart is ->")
-for x in food:
-    print(x)
-for y in price:
-    total+=y
-print(f"the total is {total}")
+#basic dialer display matrix program
+a=((1,2,3),(4,5,6),(7,8,9),("*",0,"#"))
+for collection in a:
+    for elements in collection:
+        print(elements, end = " ")
+    print()
